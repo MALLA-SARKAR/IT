ed6496344b51a54d80d869dc36daf6d1bde5e3d2
MALLA-SARKAR <aryanmallajesr@gmail.com>
2025-06-08 10:17:36 +0545

    Add files via upload
    IT

---
+++ pythonrevise.py
@@ -0,0 +1,12 @@
+# isTrue = True
+# print(isTrue)
+
+# list = [1,2]
+# print(list)
+
+# name=["Aryan","Srijan"]
+# for i in name:
+#     print(i)
+
+# oddnumber = {1,3,5,5,7,11}
+# print(f"the length is {len(oddnumber)}")
